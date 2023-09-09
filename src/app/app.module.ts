f71ea67666d07388c937b705ef39ff3e979331fd
davitGarsia <davitgarsiashvili@gmail.com>
2023-09-09 18:19:38 +0400

    add: register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  registerFormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.registerFormGroup);
+  }
+}
