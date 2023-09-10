2fd822da78e7058c7f0a24b3c1c5944a7eb4eeaf
davitGarsia <davitgarsiashvili@gmail.com>
2023-09-10 20:55:08 +0400

    add page tournamets

--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { HomeComponent } from './home/home.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: 'home', component: HomeComponent }],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'tournaments', component: TournamentsComponent },
+    ],
   },
 ];
 
