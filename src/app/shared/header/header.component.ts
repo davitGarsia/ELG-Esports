b4654977305c624d018de07e7a8633b489495365
davitGarsia <davitgarsiashvili@gmail.com>
2023-09-14 01:11:07 +0400

    refactor: correct routerlinks

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserInfoService } from 'src/app/core/interfaces/services/user-info.service';
 
 @Component({
@@ -7,12 +8,25 @@ import { UserInfoService } from 'src/app/core/interfaces/services/user-info.serv
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private userInfoService: UserInfoService) {}
+  constructor(
+    private userInfoService: UserInfoService,
+    private router: Router
+  ) {}
   // @Input() logo!: string;
   logo = '../../../assets/logos/elg-logo.png';
   image = '';
 
   ngOnInit(): void {
     this.userInfoService.image$.subscribe((image) => (this.image = image));
+  }
+
+  onSelectChange(event: any) {
+    const selectedValue = event.target.value;
+
+    if (selectedValue === 'profile') {
+      this.router.navigate(['/profile/settings']);
+    } else if (selectedValue === 'logout') {
+      this.router.navigate(['/']);
+    }
   }
 }
